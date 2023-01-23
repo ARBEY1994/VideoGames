3b460c39cc5f1802bcf7e0a3a9afb8697e744a0b
liamgabriel2021 <arbey221@gmail.com>
2023-01-23 01:12:22 -0500

    commit

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,7 @@ import dotenv from "dotenv";
 import axios from "axios";
 dotenv.config();
 
-axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
+axios.defaults.baseURL = "https://pivideogames-production.up.railway.app/";
 
 ReactDOM.render(
   <Provider store={store}>
