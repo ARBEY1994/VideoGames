264f2dfb3a67cbee5f056b4793ca48bff25f6f62
liamgabriel2021 <arbey221@gmail.com>
2023-01-23 00:06:43 -0500

    new commit

--- api/src/db.js
+++ api/src/db.js
@@ -5,7 +5,7 @@ const path = require("path");
 const modelVideog = require("./models/Videogame");
 const modelGnere = require("./models/Genres");
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
 
 let sequelize =
   process.env.NODE_ENV === "production"
@@ -13,7 +13,7 @@ let sequelize =
         database: DB_NAME,
         dialect: "postgres",
         host: DB_HOST,
-        port: 5432,
+        port: DB_PORT,
         username: DB_USER,
         password: DB_PASSWORD,
         pool: {
